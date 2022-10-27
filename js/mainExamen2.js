b7ab830e9cd743825d23ff761398805d7b861bd9
2020030275 <2020030275@upsin.edu.mx>
2022-10-27 17:26:28 -0600

    Codificacion del boton calcular

--- js/mainExamen2.js
+++ js/mainExamen2.js
@@ -1,10 +1,10 @@
 function change(){
 
-    let opc1 = document.getElementById('monedaOrigen').value;
-    let opc2 = document.getElementById('monedaDestino').value;
+    let opc1 = document.getElementById('monedaOrigen').value
+    let opc2 = document.getElementById('monedaDestino').value
 
-    let origen = document.getElementById('monedaOrigen').options; 
-    let destino = document.getElementById('monedaDestino').options;
+    let origen = document.getElementById('monedaOrigen').options
+    let destino = document.getElementById('monedaDestino').options
 
    
 
@@ -115,35 +115,100 @@ function change(){
         for (let i = 0; i <= 3; i++) {
             
 
-            destino[i] = new Option(listaSINMXN[i].text, listaSINMXN[i].value);
+            destino[i] = new Option(listaSINMXN[i].text, listaSINMXN[i].value)
             
         }
         destino.remove(4)
         
     }else if(opc1 == 'da'){
         for (let i = 0; i < 4; i++) {
-            destino[i] = new Option(listaSINDA[i].text, listaSINDA[i].value);
+            destino[i] = new Option(listaSINDA[i].text, listaSINDA[i].value)
             
         }
         destino.remove(4)
     }else if(opc1 == 'dc'){
         for (let i = 0; i < 4; i++) {
-            destino[i] = new Option(listaSINDC[i].text, listaSINDC[i].value);
+            destino[i] = new Option(listaSINDC[i].text, listaSINDC[i].value)
             
         }
         destino.remove(4)
     }else if(opc1 == 'eur'){
         for (let i = 0; i < 4; i++) {
-           destino[i] = new Option(listaSINEURO[i].text), listaSINEURO[i].value;
+           destino[i] = new Option(listaSINEURO[i].text), listaSINEURO[i].value
             
         }
         destino.remove(4)
     }
     else if(opc1 == ''){
         for (let i = 0; i <= 4; i++) {
-            destino[i] = new Option(lista[i].text, lista[i].value);
+            destino[i] = new Option(lista[i].text, lista[i].value)
             
         }
     }
+
+}
+
+function calcular(){
+
+    let cantidad = document.getElementById('cantidad').value
+    let monedaOrigen = document.getElementById('monedaOrigen').value
+    let monedaDestino = document.getElementById('monedaDestino').value
+
+    let total = 0
+
+    // Dólar americano
+    if(monedaOrigen == 'da' && monedaDestino == 'mxn'){
+        total = (cantidad * 19.85)
+    }else if(monedaOrigen == 'da' && monedaDestino == 'dc'){
+        total = cantidad * 1.35
+    }else if(monedaOrigen == 'da' && monedaDestino == 'eur'){
+        total = cantidad * 0.99
+    }
+
+    //Dólar canadiense
+    if(monedaOrigen == 'dc' && monedaDestino == 'mxn'){
+        total = (cantidad / 1.35)
+        total = (total*19.85)
+    }else if(monedaOrigen == 'dc' && monedaDestino == 'da'){
+        total = cantidad / 1.35
+    }else if(monedaOrigen == 'dc' && monedaDestino == 'eur'){
+        total = cantidad /1.35
+        total = total * 0.99
+    }
+
+    // Peso Mexicano
+    if(monedaOrigen == 'mxn' && monedaDestino == 'da'){
+        total = (cantidad / 19.85)
+    }else if(monedaOrigen == 'mxn' && monedaDestino == 'dc'){
+        total = cantidad / 19.85
+        total = total * 1.35
+    }else if(monedaOrigen == 'mxn' && monedaDestino == 'eur'){
+        total = (cantidad / 19.85)
+        total = total * 0.99
+    }
+
+    //Euro
+    if(monedaOrigen == 'eur' && monedaDestino == 'mxn'){
+        total = (cantidad * .99)
+        total = (total*19.85)
+    }else if(monedaOrigen == 'eur' && monedaDestino == 'da'){
+        total = cantidad * 0.99
+    }else if(monedaOrigen == 'eur' && monedaDestino == 'dc'){
+        total = cantidad * 0.99
+        total = total * 1.35
+    }
+
+    let subtotal = total
+    let totalCom = subtotal * 0.03
+    let totalPagar = (subtotal - totalCom)
+
+    let subTexto = document.getElementById('subtotal');
+    let tComTexto = document.getElementById('totalComision');
+    let tPagTexto = document.getElementById('totalPagar');
+
+    subTexto.innerHtml =  subTexto.setAttribute("value", subtotal)
+    tComTexto.innerHtml =  tComTexto.setAttribute("value", totalCom)
+    tPagTexto.innerHtml =  tPagTexto.setAttribute("value", totalPagar)
+
 
 }
