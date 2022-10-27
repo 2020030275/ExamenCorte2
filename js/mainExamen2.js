8fa1bf5ada6a6b21dc56495ee89de9856a0de084
2020030275 <2020030275@upsin.edu.mx>
2022-10-27 17:46:20 -0600

    Codificacion del boton registrar

--- js/mainExamen2.js
+++ js/mainExamen2.js
@@ -1,3 +1,7 @@
+let subtotalF = 0
+let totalComisionF = 0
+let totalPagarF = 0
+
 function change(){
 
     let opc1 = document.getElementById('monedaOrigen').value
@@ -202,13 +206,150 @@ function calcular(){
     let totalCom = subtotal * 0.03
     let totalPagar = (subtotal - totalCom)
 
-    let subTexto = document.getElementById('subtotal');
-    let tComTexto = document.getElementById('totalComision');
-    let tPagTexto = document.getElementById('totalPagar');
+    let subTexto = document.getElementById('subtotal')
+    let tComTexto = document.getElementById('totalComision')
+    let tPagTexto = document.getElementById('totalPagar')
 
     subTexto.innerHtml =  subTexto.setAttribute("value", subtotal)
     tComTexto.innerHtml =  tComTexto.setAttribute("value", totalCom)
     tPagTexto.innerHtml =  tPagTexto.setAttribute("value", totalPagar)
 
+
+}
+
+function calculo(){
+    let cantidad = document.getElementById('cantidad').value
+    let monedaOrigen = document.getElementById('monedaOrigen').value
+    let monedaDestino = document.getElementById('monedaDestino').value
+
+    let total = 0
+
+    // Dólar americano
+    if(monedaOrigen == 'da' && monedaDestino == 'mxn'){
+        total = (cantidad * 19.85)
+    }else if(monedaOrigen == 'da' && monedaDestino == 'dc'){
+        total = cantidad * 1.35
+    }else if(monedaOrigen == 'da' && monedaDestino == 'eur'){
+        total = cantidad * 0.99
+    }
+
+    //Dólar canadiense
+    if(monedaOrigen == 'dc' && monedaDestino == 'mxn'){
+        total = (cantidad / 1.35)
+        total = (total*19.85)
+    }else if(monedaOrigen == 'dc' && monedaDestino == 'da'){
+        total = cantidad / 1.35
+    }else if(monedaOrigen == 'dc' && monedaDestino == 'eur'){
+        total = cantidad /1.35
+        total = total * 0.99
+    }
+
+    // Peso Mexicano
+    if(monedaOrigen == 'mxn' && monedaDestino == 'da'){
+        total = (cantidad / 19.85)
+    }else if(monedaOrigen == 'mxn' && monedaDestino == 'dc'){
+        total = cantidad / 19.85
+        total = total * 1.35
+    }else if(monedaOrigen == 'mxn' && monedaDestino == 'eur'){
+        total = (cantidad / 19.85)
+        total = total * 0.99
+    }
+
+    //Euro
+    if(monedaOrigen == 'eur' && monedaDestino == 'mxn'){
+        total = (cantidad * .99)
+        total = (total*19.85)
+    }else if(monedaOrigen == 'eur' && monedaDestino == 'da'){
+        total = cantidad * 0.99
+    }else if(monedaOrigen == 'eur' && monedaDestino == 'dc'){
+        total = cantidad * 0.99
+        total = total * 1.35
+    }
+
+    let subtotal = total
+    let totalCom = subtotal * 0.03
+    let totalPagar = (subtotal - totalCom)
+
+    let resultados = [ subtotal, totalCom, totalPagar]
+
+    return resultados
+}
+
+function registro(){
+
+    let cantidad = document.getElementById('cantidad').value
+    let monedaOrigen = document.getElementById('monedaOrigen').value
+    let monedaDestino = document.getElementById('monedaDestino').value
+
+    let total = 0;
+
+    let resultados = calculo()
+    let subtotal = resultados[0]
+    let totalCom = resultados[1]
+    let totalPagar = resultados[2]
+
+    let monedaOrigenT
+    let monedaDestinoT
+
+
+
+    let lblCantidad = document.getElementById('lblCantidad');
+    let lblMonedaOrigen = document.getElementById('lblMonedaOrigen');
+    let lblMonedaDestino = document.getElementById('lblMonedaDestino');
+    let lblSubtotal = document.getElementById('lblSubtotal');
+    let lblTotalComision = document.getElementById('lblTotalComision');
+    let lblTotalPagar = document.getElementById('lblTotalPagar');
+
+
+    if (monedaOrigen == 'mxn') {
+        monedaOrigenT = "Peso mexicano";
+    }else if (monedaOrigen == 'da') {
+        monedaOrigenT = "Dólar americano";
+    }else if (monedaOrigen == 'dc') {
+        monedaOrigenT = "Dólar canadiense";
+    }else if (monedaOrigen == 'eur') {
+        monedaOrigenT = "Euro";
+    }
+
+
+    if (monedaDestino == 'mxn') {
+        monedaDestinoT = "Peso mexicano";
+    }else if (monedaDestino == 'da') {
+        monedaDestinoT = "Dólar americano";
+    }else if (monedaDestino == 'dc') {
+        monedaDestinoT = "Dólar canadiense";
+    }else if (monedaDestino == 'eur') {
+        monedaDestinoT = "Euro";
+    }
+
+
+    // Reemplazo
+    lblCantidad.innerHTML = lblCantidad.innerHTML + cantidad + "<br>";
+    lblMonedaOrigen.innerHTML = lblMonedaOrigen.innerHTML + monedaOrigenT + "<br>";
+    lblMonedaDestino.innerHTML = lblMonedaDestino.innerHTML + monedaDestinoT + "<br>";
+    lblSubtotal.innerHTML = lblSubtotal.innerHTML + subtotal + "<br>";
+    lblTotalComision.innerHTML = lblTotalComision.innerHTML + totalCom + "<br>";
+    lblTotalPagar.innerHTML = lblTotalPagar.innerHTML + totalPagar + "<br>";
+
+
+    // Suma registros
+    let regSubtotal = document.getElementById('registroSubtotal');
+    let regTotalComision = document.getElementById('registroTotalComision');
+    let regTotalPagar = document.getElementById('registroTotalPagar');
+
+    subtotalF = subtotalF + subtotal;
+
+    regSubtotal.innerText = ""
+    regSubtotal.innerText = regSubtotal.innerText + subtotalF;
+
+    totalComisionF = totalComisionF + totalCom;
+
+    regTotalComision.innerText = ""
+    regTotalComision.innerText = regTotalComision.innerText + totalComisionF;
+
+    totalPagarF = totalPagarF + totalPagar;
+
+    regTotalPagar.innerText = ""
+    regTotalPagar.innerText = regTotalPagar.innerText + totalPagarF;
 
 }
